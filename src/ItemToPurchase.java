908fdbea99d9b0b82c1433d66d1fb8020a081238
denizyilmaz <denizyilmaz@localhost>
2020-10-22 22:24:18 -0400

    default

---
+++ src/ItemToPurchase.java
@@ -0,0 +1,13 @@
+
+public class ItemToPurchase {
+String itemName;
+int itemPrice;
+int itemQuantity;
+public static void main(String[] args) {
+	
+}
+ItemToPurchase(String itemName){
+	ItemToPurchase it = new ItemToPurchase();
+	
+}
+}
