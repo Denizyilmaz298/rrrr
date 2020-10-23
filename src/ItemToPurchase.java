c75b18e97a1fc9fb8b685c947955195794a454ff
denizyilmaz <denizyilmaz@localhost>
2020-10-22 23:32:13 -0400

    adsfd

--- src/ItemToPurchase.java
+++ src/ItemToPurchase.java
@@ -1,6 +1,24 @@
 
 public class ItemToPurchase {
 String itemName;
+public String getItemName() {
+	return itemName;
+}
+public void setItemName(String itemName) {
+	this.itemName = itemName;
+}
+public int getItemPrice() {
+	return itemPrice;
+}
+public void setItemPrice(int itemPrice) {
+	this.itemPrice = itemPrice;
+}
+public int getItemQuantity() {
+	return itemQuantity;
+}
+public void setItemQuantity(int itemQuantity) {
+	this.itemQuantity = itemQuantity;
+}
 int itemPrice;
 int itemQuantity;
 public static void main(String[] args) {
